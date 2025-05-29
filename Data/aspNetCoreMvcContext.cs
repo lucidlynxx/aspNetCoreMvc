8604c043f66c0df5e885ef50e778bcfe6cecc815
lucidlynxx <dzakysyahrizal04@gmail.com>
2025-05-30 05:32:50 +0700

    initial commit

---
+++ Data/aspNetCoreMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using aspNetCoreMvc.Models;
+
+namespace aspNetCoreMvc.Data
+{
+    public class aspNetCoreMvcContext : DbContext
+    {
+        public aspNetCoreMvcContext(DbContextOptions<aspNetCoreMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspNetCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
